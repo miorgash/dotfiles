6f1446d3a9140f287a2a91abb70938aac3a97438
Miori Igarashi <miori@MiorinoMacBook-Pro.local>
2019-10-02 22:50:14 +0900

    add .zshrc setting for nvim alias

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo 'export XDG_CONFIG_HOME="~/.config"' >> ~/.zshenv
-echo 'export REPOSITORIES_HOME="~/Repositories"' >> ~/.zshenv
+echo 'export XDG_CONFIG_HOME="${HOME}/.config"' >> ~/.zshenv
+echo 'export REPOSITORIES_HOME="${HOME}/Repositories"' >> ~/.zshenv
+echo 'alias vim=nvim' >> ~/.zshrc
