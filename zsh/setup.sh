aa0d87ce62bc337e5049a7a34b42be45039d7fd4
Miori Igarashi <miori@MiorinoMacBook-Pro.local>
2019-10-12 20:02:52 +0900

    move word_vector_path into $PATH

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -2,5 +2,5 @@
 echo 'export REPOSITORIES_HOME="${HOME}/Repositories"' >> ~/.zshenv
 echo 'export XDG_CONFIG_HOME="${HOME}/.config"' >> ~/.zshenv
 echo 'export JUPYTER_CONFIG_HOME="${HOME}/.jupyter"' >> ~/.zshenv
-echo 'export WORD_VECTOR_HOME="${HOME}/WordVector"' >> ~/.zshenv
+echo 'export PATH="${HOME}/WordVector:$PATH"' >> ~/.zshenv
 echo 'alias vim=nvim' >> ~/.zshrc
