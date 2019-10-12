e1bb934a622d71c784b8b90c12e827fe1ed67abf
Miori Igarashi <miori@MiorinoMacBook-Pro.local>
2019-10-12 20:34:54 +0900

    remove word vector setting

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -2,5 +2,4 @@
 echo 'export REPOSITORIES_HOME="${HOME}/Repositories"' >> ~/.zshenv
 echo 'export XDG_CONFIG_HOME="${HOME}/.config"' >> ~/.zshenv
 echo 'export JUPYTER_CONFIG_HOME="${HOME}/.jupyter"' >> ~/.zshenv
-echo 'export PATH="${HOME}/WordVector:$PATH"' >> ~/.zshenv
 echo 'alias vim=nvim' >> ~/.zshrc
