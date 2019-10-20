ae8904dfb758bec959e6807416ad404d79053d36
Miori Igarashi <miori@MiorinoMacBook-Pro.local>
2019-10-20 15:46:30 +0900

    add nbextensions setup, change dir structure

--- mac/jupyter/setup.sh
+++ mac/jupyter/setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# Extensions
+pipenv run jupyter contrib nbextension install --user
+pipenv run jupyter nbextensions_configurator enable --user
+
+# Font
 mkdir ${JUPYTER_CONFIG_HOME}/custom
-ln -s ${REPOSITORIES_HOME}/dotfiles/jupyter/custom.css ${JUPYTER_CONFIG_HOME}/custom/custom.css
+ln -sf ${REPOSITORIES_HOME}/dotfiles/mac/jupyter/custom.css ${JUPYTER_CONFIG_HOME}/custom/custom.css
