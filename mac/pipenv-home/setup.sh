c69f58d4d5f6899e7d60c1c1b7f712dcb1b03489
Miori Igarashi <miori@MiorinoMacBook-Pro.local>
2019-12-17 20:19:56 +0900

    delete pipenv-home/setup.sh

--- mac/pipenv-home/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Create virtual-env in home directory
-cd $HOME
-pipenv install --skip-lock
-# GUI Configurator
-# home の仮想環境の jupyter コマンドを使いたいだけで
-# 設定自体は仮想環境をまたいで有効なはず
-pipenv run jupyter nbextensions_configurator enable --user
