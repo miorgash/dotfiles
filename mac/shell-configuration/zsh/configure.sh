1fba19641f295d37c1bd7b3e6111fd3eacd1abc2
Miori Igarashi <miori@MiorinoMacBook-Pro.local>
2019-12-21 18:00:22 +0900

    good-bye to pyenv

--- mac/shell-configuration/zsh/configure.sh
+++ mac/shell-configuration/zsh/configure.sh
@@ -14,12 +14,6 @@ echo 'export XDG_CONFIG_HOME="${HOME}/.config"' >> ~/.zshenv
 echo '\n# nvim settings' >> ~/.zshrc
 echo 'alias vim=nvim' >> ~/.zshrc
 
-# pyenv
-echo '\n# pyenv settings' >> ~/.zshenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshenv
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshenv
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshenv
-
 # pipx
 echo '\n# pipx settings' >> ~/.zshenv
 echo 'export PATH=/Users/miori/.local/bin:${PATH}' >> ~/.zshenv
