9d5ff8e06f34cdbf5957810bcd4260f903955a02
Miori Igarashi <miori@MiorinoMacBook-Pro.local>
2019-12-21 18:15:09 +0900

    change anyway

--- mac/configure/zsh/configure.sh
+++ mac/configure/zsh/configure.sh
@@ -6,18 +6,17 @@
 
 # user settings
 echo '\n# user settings' >> ~/.zshenv
-echo 'export REPOSITORIES_HOME="${HOME}/Repositories"' >> ~/.zshenv
-echo 'export JUPYTER_CONFIG_HOME="${HOME}/.jupyter"' >> ~/.zshenv
+echo 'export REPOS_HOME="${HOME}/Repos"' >> ~/.zshenv
+echo 'export JUPYTER_HOME="${HOME}/.jupyter"' >> ~/.zshenv
 echo 'export ENV_DIR="${HOME}/.local/venv"' >> ~/.zshenv
 
 # for neovim etc.
 echo '\n# nvim settings' >> ~/.zshenv
 echo 'export XDG_CONFIG_HOME="${HOME}/.config"' >> ~/.zshenv
-echo '\n# nvim settings' >> ~/.zshrc
 
 # pipx
 echo '\n# pipx settings' >> ~/.zshenv
-echo 'export PATH=/Users/miori/.local/bin:${PATH}' >> ~/.zshenv
+echo 'export PATH="${HOME}/.local/bin:${PATH}"' >> ~/.zshenv
 
 # **
 # aliases
