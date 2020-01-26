873c9a4cd74466214958dff4605d130157b7968f
Miori Igarashi <miori@MiorinoMacBook-Pro.local>
2020-01-26 12:42:32 +0900

    remove files currently not in use

--- pipx/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python3 -m pipx install jupyter --include-deps
-python3 -m pipx install jupyter-nbextensions-configurator --include-deps
-python3 -m pipx install jupyter-contrib-nbextensions --include-deps
-python3 -m pipx install rise --include-deps
-python3 -m pipx install awscli --include-deps
-python3 -m pipx install ipython --include-deps
--- venv/ipython/configure.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source ${HOME}/.local/pipx/venvs/ipython/bin/activate
-pip3 install -r requirements.txt
