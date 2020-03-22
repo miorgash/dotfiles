d924df13fa556de0bcf395a45177ff7eb20445c8
Miori Igarashi <miori@MiorinoMacBook-Pro.local>
2020-03-22 13:43:06 +0900

    change init.vims path

--- osx/nvim/configure.sh
+++ osx/nvim/configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ${XDG_CONFIG_HOME}/nvim/colors
-ln -s ${REPOS_HOME}/dotfiles/nvim/init.vim ${XDG_CONFIG_HOME}/nvim/init.vim
+ln -s ${REPOS_HOME}/dotfiles/init.vim ${XDG_CONFIG_HOME}/nvim/init.vim
 git clone https://github.com/cocopon/iceberg.vim/ ${REPOS_HOME}/iceberg.vim
 cp ${REPOS_HOME}/iceberg.vim/colors/iceberg.vim ${XDG_CONFIG_HOME}/nvim/colors/iceberg.vim
 
