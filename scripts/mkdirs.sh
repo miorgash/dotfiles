0c96977363f673f275cf9215699cd04a8440759f
miorgash <miorgash@gmail.com>
2020-10-08 00:56:17 +0000

    add assets dir

--- scripts/mkdirs.sh
+++ scripts/mkdirs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mkdir -p \
-	~/analysis/adhoc/containers \
-	~/analysis/scripts \
-	~/analysis/pipelines\
-	~/analysis/pkg\
-	~/analysis/app\
-	~/training/algorithms \
-	~/training/libraries \
-	~/training/teaching
+	~/assets/analysis/adhoc/containers \
+	~/assets/analysis/scripts \
+	~/assets/analysis/pipelines\
+	~/assets/analysis/pkg\
+	~/assets/analysis/app\
+	~/assets/training/algorithms \
+	~/assets/training/libraries \
+	~/assets/training/teaching
